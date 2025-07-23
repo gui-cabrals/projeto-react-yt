9521e887ff59efdae376f885c3de914ad80a607c
gui-cabrals <cabral.guilherme@icloud.com>
2025-07-23 15:45:49 -0300

    check icon ao completar a tarefa

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button.jsx";
 
@@ -19,10 +19,11 @@ function Tasks(props) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.onTaskClick(task.id)}
-            className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${
+            className={`bg-slate-400 w-full flex items-center gap-2 text-white p-2 rounded-md text-left ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button
